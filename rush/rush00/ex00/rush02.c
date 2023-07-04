72787a934997144deca65af720b40b486535154b
etovaz <etovaz@Ubuntu42.myguest.virtualbox.org>
2023-07-04 10:34:13 -0300

    adicionando as variações dos exercicios, o original que eu precisei fazer foi o rush03

---
+++ rush/rush00/ex00/rush02.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etovaz <etovaz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 09:18:31 by etovaz            #+#    #+#             */
+/*   Updated: 2023/07/04 10:31:05 by etovaz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y);
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	fill_corners(int x, int y, int lin)
+{
+	int	col;
+
+	col = 1;
+	while (col != x + 1)
+	{
+
+		if (lin == 1 && (col == 1 || col == x))
+			ft_putchar('A');
+		else if (lin == y && (col == 1 || col == x))
+			ft_putchar('C');
+		else if (lin == 1 || lin == y)
+			ft_putchar('B');
+		col++;
+	}
+}
+
+void	fill_middle(int x, int y, int lin)
+{
+	int	col;
+
+	col = 1;
+	while (col != x + 1)
+	{
+		if (lin != 1 && lin != y)
+		{
+			if ((col == 1 || col == x))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+		}
+		col++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	lin;
+
+	lin = 1;
+	if (x <= 0 || y <= 0)
+		return ;
+	while (lin != y + 1)
+	{
+		fill_corners(x, y, lin);
+		fill_middle(x, y, lin);
+		lin++;
+		ft_putchar('\n');
+	}
+}
+
+int	main(void)
+{
+	rush(4, 4);
+	rush(1, 1);
+	rush(0, 4);
+	rush(-4, 4);
+	rush(4, -4);
+	rush(5, 3);
+	rush(1, 5);
+	rush(5, 1);
+	return (0);
+}
