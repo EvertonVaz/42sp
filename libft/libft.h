5284fc01b587b7caf6dd3b5f32bcd5017006f856
Everton <egeraldo@student.42sp.org.br>
2023-07-18 18:58:42 -0300

    incluindo ft_strlcat

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,14 @@
 /*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 20:16:53 by egeraldo          #+#    #+#             */
-/*   Updated: 2023/07/18 18:31:56 by egeraldo         ###   ########.fr       */
+/*   Updated: 2023/07/18 18:46:29 by egeraldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
+#include <stddef.h>
 
 int				ft_atoi(char *str);
 int				ft_isalnum(int c);
@@ -29,4 +29,6 @@ void			*ft_memset(void *s, int c, size_t n);
 void			ft_bzero(void *buff, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
+size_t			strlcat(char *dst, const char *src, size_t n);
+
 #endif
