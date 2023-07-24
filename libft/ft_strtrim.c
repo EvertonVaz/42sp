fe8ab5df118edd8342b353e636e8499e4e6add45
Everton <egeraldo@student.42sp.org.br>
2023-07-24 18:24:54 -0300

    ft_strtrim feita e testada

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,10 +6,41 @@
 /*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:41:16 by egeraldo          #+#    #+#             */
-/*   Updated: 2023/07/24 15:46:16 by egeraldo         ###   ########.fr       */
+/*   Updated: 2023/07/24 18:22:51 by egeraldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set);
+static int	ft_is_in(char c, const char *str)
+{
+	while (*str)
+	{
+		if (*str++ == c)
+			return (1);
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*result;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_is_in(s1[start], set))
+		start++;
+	while (end > start && ft_is_in(s1[end], set))
+		end--;
+	if (++end > start)
+		result = (char *)ft_calloc((end - start + 1), sizeof(char));
+	else
+		result = (char *)ft_calloc(1, sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	s1 += start;
+	ft_strlcpy(result, s1, (end - start + 1));
+	return (result);
+}
