d17a5e4d0fe84738c7f909e02d65806799ecb89d
Everton <egeraldo@student.42sp.org.br>
2023-08-02 11:32:24 -0300

    ft_lstiter itera a lista lst e aplica a função f no conteúdo de cada nó. O parâmetro lst é um ponteiro para o primeiro nó da lista. O parâmetro f é um ponteiro para uma função que é usada para iterar na lista.
    A função ft_lstiter faz o seguinte:
    
    Percorre a lista.
    Chama a função f no conteúdo de cada nó.
    A função ft_lstiter é uma função muito útil para aplicar uma função a cada nó de uma lista.

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 20:16:53 by egeraldo          #+#    #+#             */
-/*   Updated: 2023/07/28 16:49:07 by egeraldo         ###   ########.fr       */
+/*   Updated: 2023/08/02 11:26:57 by egeraldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ t_list		*ft_lstlast(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void*));
 void		ft_lstclear(t_list **lst, void (*del)(void*));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
