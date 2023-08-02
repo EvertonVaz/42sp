6e564452ef80f21bc6f8f22ffc3350f0c3da93ee
Everton <egeraldo@student.42sp.org.br>
2023-08-02 12:15:44 -0300

    ft_lstmap itera a lista lst e aplica a função f no conteúdo de cada nó. Cria uma nova lista resultante das sucessivas aplicações da função f. A função del é usada para deletar o conteúdo de um nó, caso a criacao do novo no nao funcione
    O parâmetro lst é um ponteiro para o primeiro nó da lista.
    O parâmetro f é um ponteiro para uma função que é usada para iterar na lista.
    O parâmetro del é um ponteiro para uma função que é usada para deletar o conteúdo de um nó,
    	caso o novo no criador nao consiga alocar memoria
    
    A função ft_lstmap faz o seguinte:
    
    Percorre a lista.
    Chama a função f no conteúdo de cada nó.
    Cria uma nova lista com os resultados da função f

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 20:16:53 by egeraldo          #+#    #+#             */
-/*   Updated: 2023/08/02 11:26:57 by egeraldo         ###   ########.fr       */
+/*   Updated: 2023/08/02 11:46:26 by egeraldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void*));
 void		ft_lstclear(t_list **lst, void (*del)(void*));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
