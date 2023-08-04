541c36eb99011882028a61e988a6c2453bf750dc
Everton <egeraldo@student.42sp.org.br>
2023-08-03 21:15:44 -0300

    Segunda versaozinha do danado, tem 26 linhas mas esta escrevendo, preciso arrumar mas esta retornando uma linha por vez

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,35 +6,11 @@
 /*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 15:47:16 by egeraldo          #+#    #+#             */
-/*   Updated: 2023/08/03 20:56:18 by egeraldo         ###   ########.fr       */
+/*   Updated: 2023/08/03 21:15:11 by egeraldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-/*
-eu preciso ler até encontrar o '\n' que será o final da linha
-caso exista um \n no texto do arquivo ele estara como '\\n'
-entao eu preciso criar um condição de leitura ate chegar nesse caso especifico
-apos printar a proxima linha
-
-caso seja de 1 em 1 a gente consegue verificar o caso, caso nao seja como faremos?
-*/
-
-/* void test()
-{
-
-	while (*buffer != NULL && *buffer != '\n')
-	{
-		write(1, buffer, 1);
-		buffer++;
-		i++;
-	}
-	if (*buffer == '\n' && *++buffer != NULL)
-		write(1, "\n", 1);
-} */
-
-
 
 char	*get_next_line(int fd)
 {
@@ -47,22 +23,29 @@ char	*get_next_line(int fd)
 	line = ft_calloc(1, 1);
 	while (++i <= BUFFER_SIZE)
 	{
-		buffer = (char *)ft_calloc(BUFFER_SIZE * i, sizeof(char));
-		read(fd, buffer, BUFFER_SIZE * i);
-		line = ft_strjoin(line, *&buffer);
-		if (ft_strchr(line, '\n'))
-		{
-			index = ft_strlen(line) - ft_strlen(ft_strchr(line, '\n')) + 1;
-			line[index] = '\0';
-			buffer = ft_strchr(buffer, '\n');
-			return (line);
-		}
+		if (buffer == NULL)
+			buffer = (char *)ft_calloc(BUFFER_SIZE * i, sizeof(char));
+		else
+			{
+				line = ft_strjoin(line, *&buffer);
+				if (ft_strchr(line, '\n'))
+				{
+					index = ft_strlen(line) - ft_strlen(ft_strchr(line, '\n')) + 1;
+					line[index] = '\0';
+					buffer = ft_strchr(buffer, '\n') + 1;
+					return (line);
+				}
+				else
+					read(fd, buffer, BUFFER_SIZE * i);
+			}
 	}
 	return (NULL);
 }
 
+#include <stdio.h>
 int main()
 {
-	get_next_line(open("test", O_RDONLY));
-	get_next_line(open("test", O_RDONLY));
+	int fd = open("test", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s",get_next_line(fd));
 }
